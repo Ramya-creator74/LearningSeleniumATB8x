2100d73dc68aa77e9629e70b735820c6151a147c
Ramya-creator74 <chitturiramyanri@gmail.com>
2024-12-21 17:55:40 +0530

    Initial commit

---
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium_03.java
@@ -0,0 +1,21 @@
+package com.thetestingacademy.ex01_18122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class Selenium_02 {
+
+    @Test
+    public void test_Selenium(){
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+
+
+    }
+
+
+}
